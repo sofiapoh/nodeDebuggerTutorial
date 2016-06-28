42498d8dfbd77ed6608f4056dc7544773792c64f
Sofia <emmapohjalainen@gmail.com>
2016-06-28 15:57:10 +0100

    first commit

---
+++ tutorial/src/handler.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+const handler = (req, res) => {
+    const url = req.url;
+    if(url === '/'){
+        fs.readFile(__dirname + '/../public/index.html', (err, data) =>{
+            if(err) throw err;
+            res.writeHead(200, {'Content-type': 'text/html'});
+            res.end(data);
+        });
+    } else if (url.indexOf('.') > -1){
+    
+        const ext = url.split('.')[1];
+
+        fs.readFile(__dirname + "/../public" + url, (err, data) => {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-type': 'text/' + ext });
+            res.end(data);
+        });
+    } else {
+        res.writeHead(404);
+        res.end(`Sorry page you requested isn't here`);
+    }
+}
+
+
+module.exports = handler;
