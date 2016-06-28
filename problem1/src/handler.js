a2db0c98c45b39ebeccb2a908775ff5fedc08061
mattiaS <mattia@3doers.it>
2016-06-28 19:26:36 +0100

    better the code

--- problem1/src/handler.js
+++ problem1/src/handler.js
@@ -1,24 +1,26 @@
+'use strict'
 const fs = require('fs');
 
-const handler = (req, res) => {
-    const url = req.url;
-    if (url=== '/'){
-        fs.readFile(__dirname + "../public/index.html", (err, data) => {
-            if (err) throw err;
-            res.writeHead(200, { 'Content-type': 'text/html' });
-            res.end(data);
-        });
-    } else if (url.indexOf('.') > -1){
-        const ext = url.split('.')[1];
-        fs.readFile(__dirname + "/../public" + url, (err, data) => {
-            if (err) throw err;
-            res.writeHead(200, { 'Content-type': 'text/' + ext });
-            res.end(data);
-        });
-    } else {
-        res.writeHead(404);
-        res.end(`Sorry page you requested isn't here`);
-    }
+function handler (req, res)  {
+	const url = req.url;
+
+	if (url=== '/'){
+		fs.readFile(__dirname + "../public/index.html", (err, data) => {
+			res.writeHead(200, { 'Content-type': 'text/html' });
+			res.end(data);
+		});
+	} else if (url.indexOf('.') > -1){
+		const ext = url.split('.')[1];
+		fs.readFile(__dirname + "/../public" + url, (err, data) => {
+			if (err){
+				res.writeHead(404)	
+				res.end('<h2> page not found </h2>')
+			}else { 
+				res.writeHead(200, { 'Content-type': 'text/' + ext });
+				res.end(data);
+			}
+		});
+	} 
 }
 
 
--- problem3/src/handler.js
+++ problem3/src/handler.js
@@ -1,26 +1,26 @@
+'use strict'
 const fs = require('fs');
 
 const handler = (req, res) => {
     const url = req.url;
     if (url=== '/'){
-        fs.readFile(__dirname + "/../public/index.html", (err, data) => {
+        fs.readFile(__dirname + '/../public/index.html', (err, data) => {
             if (err) throw err;
             res.writeHead(200, { 'Content-type': 'text/html' });
             res.end(data);
-
         });
     } else if (url.indexOf('.') > -1){
         const ext = url.split('.')[1];
-        fs.readFile(__dirname + "/../public" + url, (err, data) => {
-            if (err) throw err;
+        fs.readFile(__dirname + '/../public' + url, (err, data) => {
+		if(err){
+			res.writeHead(404);
+			res.end(`Sorry page you requested isn't here`);
+		}
             res.writeHead(200, { 'Content-type': 'text/' + ext });
             res.end(data);
         });
     } else {
-        res.writeHead(404);
-        res.end(`Sorry page you requested isn't here`);
-    }
+            }
 }
-
 
 module.exports = handler;
